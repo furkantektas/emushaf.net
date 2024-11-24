fe7295e2165874727e3118bd27b8ff91ae71a3ca
Furkan Tektas <tektasfurkan@gmail.com>
2024-11-24 11:27:31 +0000

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://emushaf.net',
+    generateRobotsTxt: false,
+    changefreq: 'yearly',
+    priority: 1.0,
+    outDir: 'out',
+    exclude: [
+        '/up',
+        '/manifest.webmanifest',
+        '/settings/',
+    ],
+    sitemapSize: 10000
+}
